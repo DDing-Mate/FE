8f50d36961451b7dd9c80669e432cc4a9fb7962e
qwer0114 <skyok203@naver.com>
2024-02-02 15:49:44 +0900

    feat: 폴더구조 정의

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
+import Login from "./pages/login";
+import SignUp from "./pages/singup";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />}></Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/singup" element={<SignUp />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/singup/index.jsx
@@ -0,0 +1,5 @@
+function SignUp() {
+  return <div>sigup</div>;
+}
+
+export default SignUp;
