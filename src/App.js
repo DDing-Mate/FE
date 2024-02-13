e9328419e64267c22ab06cac62d81ffef79632c5
qwer0114 <skyok203@naver.com>
2024-02-13 17:52:23 +0900

    feat: 글쓰기 라우트

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import Login from "./pages/login";
 import SignUp from "./pages/signup";
+import Post from "./pages/post";
 import "./index.css";
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
             <Route path="login" element={<Login />} />
           </Route>
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/write" element={<Post />} />
         </Routes>
       </BrowserRouter>
     </div>
