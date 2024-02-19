703f25ab2379c6b27183ecb7cd2054cf619d1190
qwer0114 <skyok203@naver.com>
2024-02-19 16:18:45 +0900

    feat: msw handler

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,6 +1,26 @@
 import { HttpResponse, http } from "msw";
 import { postDetail, posts } from "./dummy";
 export const handlers = [
+  http.post("http://api.example.com/account/login", async ({ request }) => {
+    const { email, password } = await request.json();
+    console.log(email);
+
+    if (email === "testuser" && password === "testpassword") {
+      return HttpResponse.json(
+        {
+          token: "fake_token",
+        },
+        { status: 201 }
+      );
+    } else {
+      return HttpResponse.json(
+        {
+          errorMessage: "Not Authorized",
+        },
+        { status: 401 }
+      );
+    }
+  }),
   http.get("http://api.example.com/post/all", () => {
     return HttpResponse.json(posts);
   }),
