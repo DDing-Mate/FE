9148f68e997f1f2681d33937567e9efecb833160
qwer0114 <skyok203@naver.com>
2024-02-19 16:20:22 +0900

    feat: api 요청

--- src/pages/home/components/PostcardsContainer.jsx
+++ src/pages/home/components/PostcardsContainer.jsx
@@ -1,26 +1,27 @@
 import React, { useState, useEffect } from "react";
 import Postcard from "./Postcard";
-import { getPosts } from "../../../api";
+import { getMajor, getPosts } from "../../../api";
 import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 function PostcardsContainer() {
   const { data, error, isLoading } = useQuery({
     queryKey: ["posts"],
     queryFn: getPosts,
   });
+
   console.log(data);
-  console.log(isLoading);
-  return (
-    <div className="flex flex-wrap -m-4">
-      {data?.data.map((post) => (
-        <div
-          key={post.postId}
-          className="p-4 w-full sm:w-1/2 md:w-1/3 lg:w-1/4"
-        >
-          <Postcard data={post} />
-        </div>
-      ))}
-    </div>
-  );
+  // return (
+  //   // <div className="flex flex-wrap -m-4">
+  //   //   {data?.data.map((post) => (
+  //   //     <div
+  //   //       key={post.postId}
+  //   //       className="p-4 w-full sm:w-1/2 md:w-1/3 lg:w-1/4"
+  //   //     >
+  //   //       <Postcard data={post} />
+  //   //     </div>
+  //   //   ))}
+  //   // </div>
+  // );
 }
 
 export default PostcardsContainer;
