095b63544826f4e775054c32ad3a80702b00cb97
qwer0114 <skyok203@naver.com>
2024-02-21 20:33:45 +0900

    feat: 모 분

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -5,7 +5,7 @@ import { useForm } from "react-hook-form";
 import { useMutation } from "@tanstack/react-query";
 import { login } from "../../api";
 import { useCookies } from "react-cookie";
-function Login({ setVisible }) {
+function Login() {
   const navigate = useNavigate();
   const { register, handleSubmit } = useForm();
   const { mutate } = useMutation({ mutationFn: login, mutationKey: ["login"] });
@@ -24,39 +24,37 @@ function Login({ setVisible }) {
     });
   };
   return (
-    <Modal closeModal={() => setVisible(false)}>
-      <div className="modal-box w-[600px]">
-        <h3 className="font-bold text-lg">로그인</h3>
-        <div className="modal-action w-full float-left">
-          <form
-            className="w-full flex flex-col items-center"
-            onSubmit={handleSubmit((data) => submit(data))}
+    <div className="modal-box w-[600px]">
+      <h3 className="font-bold text-lg">로그인</h3>
+      <div className="modal-action w-full float-left">
+        <form
+          className="w-full flex flex-col items-center"
+          onSubmit={handleSubmit((data) => submit(data))}
+        >
+          <Input
+            className="input input-bordered w-96 mb-3"
+            label={"이메일"}
+            placeholder={"123@mju.ac.kr"}
+            htmlFor={"email"}
+            {...register("email")}
+          />
+          <Input
+            className="input input-bordered w-96 mb-3"
+            label={"비밀번호"}
+            type="password"
+            htmlFor={"password"}
+            {...register("password")}
+          />
+          <button className="btn btn-wide my-6">로그인</button>
+          <Link
+            to={"/signup"}
+            className="text-red-500 cursor-pointer hover:underline"
           >
-            <Input
-              className="input input-bordered w-96 mb-3"
-              label={"이메일"}
-              placeholder={"123@mju.ac.kr"}
-              htmlFor={"email"}
-              {...register("email")}
-            />
-            <Input
-              className="input input-bordered w-96 mb-3"
-              label={"비밀번호"}
-              type="password"
-              htmlFor={"password"}
-              {...register("password")}
-            />
-            <button className="btn btn-wide my-6">로그인</button>
-            <Link
-              to={"/signup"}
-              className="text-red-500 cursor-pointer hover:underline"
-            >
-              아직 회원이 아니신가요? 회원가입하러 가기
-            </Link>
-          </form>
-        </div>
+            아직 회원이 아니신가요? 회원가입하러 가기
+          </Link>
+        </form>
       </div>
-    </Modal>
+    </div>
   );
 }
 
